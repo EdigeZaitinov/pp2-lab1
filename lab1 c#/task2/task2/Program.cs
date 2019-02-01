8c2d46308d65c23955922c51ddf8fa9992ae5b50
EdigeZaitinov <47225219+EdigeZaitinov@users.noreply.github.com>
2019-02-02 02:54:01 +0600

    Add files via upload

---
+++ lab1 c#/task2/task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace task2
+{
+    public class Student //Class Student
+    {
+        public String Name; // details of Class
+        public String ID;
+        public int Year;
+
+
+        public Student(string Id, string name, int year) // Instance constructor
+        {
+            Name = name; // details of constructor
+            ID = Id;
+            Year = year;
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Student stud = new Student(" ", " ", 2); //Creating a new Student object
+            stud.ID = Console.ReadLine(); //Input
+            stud.Name = Console.ReadLine();
+            stud.Year = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(stud.ID + " " + stud.Name + " " + stud.Year); //output  
+        }
+    }
+}
---
+++ lab1 c#/task3/task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace task3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n, i;
+            n = Convert.ToInt16(Console.ReadLine());
+            int[] ar = new int[n];
+            for (i = 0; i < n; i++)
+            {
+                ar[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            for (i = 0; i < n; i++)
+            {
+                Console.Write($"{ar[i]} {ar[i]} "); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
